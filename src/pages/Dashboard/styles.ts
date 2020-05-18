1b47eb6a050e54587b47a95fa61bea6e5ebfb316
Rubens Pereira do Nascimento <iorubenspn@gmail.com>
2020-05-18 06:13:36 -0300

    feat: integrations of backend

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -18,9 +18,13 @@ export const Title = styled.h1`
 
 export const CardContainer = styled.section`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(1, 1fr);
   grid-gap: 32px;
   margin-top: -150px;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 `;
 
 export const Card = styled.div`
